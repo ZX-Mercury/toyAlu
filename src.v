ca4421eb6f847dbfcb294f9f4ecfb9d0f09e949a
ZX-Mercury <144932310+ZX-Mercury@users.noreply.github.com>
2024-12-08 17:05:57 +0800

    Update src.v

--- src.v
+++ src.v
@@ -1,27 +1,79 @@
-module Add(
-
-    input       [31:0]          a,
-
-    input       [31:0]          b,
-
-    output reg  [31:0]          sum
-
+module Add4(
+    input  [3:0]               a,
+    input  [3:0]               b,
+    input                      ci,
+    output  [3:0]              sum,
+    output                     co
 );
+    wire [3:0] p = a ^ b;
+    wire [3:0] g = a & b;
+    wire [3:0] c;
+    assign c[0] = ci;
+    assign c[1] = g[0] | (p[0] & c[0]);
+    assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & c[0]);
+    assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & c[0]);
+    assign co = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & c[0]);
+    assign sum = p ^ c;
+endmodule
 
-    reg[32:0] carry = 32'b0;
+module Add16(
+    input  [15:0]               a,
+    input  [15:0]               b,
+    input                       ci,
+    output  [15:0]              sum,
+    output                      co
+);
+    wire [3:0] c;
+    assign c[0] = ci;
+    Add4 add4_0(
+        .a(a[3:0]),
+        .b(b[3:0]),
+        .ci(c[0]),
+        .sum(sum[3:0]),
+        .co(c[1])
+    );
+    Add4 add4_1(
+        .a(a[7:4]),
+        .b(b[7:4]),
+        .ci(c[1]),
+        .sum(sum[7:4]),
+        .co(c[2])
+    );
+    Add4 add4_2(
+        .a(a[11:8]),
+        .b(b[11:8]),
+        .ci(c[2]),
+        .sum(sum[11:8]),
+        .co(c[3])
+    );
+    Add4 add4_3(
+        .a(a[15:12]),
+        .b(b[15:12]),
+        .ci(c[3]),
+        .sum(sum[15:12]),
+        .co(co)
+    );
+endmodule
 
-    integer i;
-
-    always @(*) begin
-
-        for (i = 0; i < 32; i = i + 1) begin
-
-        {carry[i+1], sum[i]} = a[i] + b[i] + carry[i];
-
-        end
-
-    end
-
-    
-
+module Add(
+    input [31:0]               a,
+    input [31:0]               b,
+    output [31:0]              sum
+);
+    wire tmp;
+    wire [15:0] sum0, sum1;
+    Add16 add16_0(
+        .a(a[15:0]),
+        .b(b[15:0]),
+        .ci(1'b0),
+        .sum(sum[15:0]),
+        .co(tmp)
+    );
+    Add16 add16_1(
+        .a(a[31:16]),
+        .b(b[31:16]),
+        .ci(tmp),
+        .sum(sum[31:16])
+        .co()
+    );
 endmodule
